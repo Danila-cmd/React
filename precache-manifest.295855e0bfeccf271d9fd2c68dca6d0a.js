c75fe2edc93867fcd025b1f67ed08571922e52f7
Danila <dvfeduleev@gmail.com>
2020-10-13 14:16:39 +0300

    Updates

---
+++ precache-manifest.295855e0bfeccf271d9fd2c68dca6d0a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ff63e08bd6b431da8f1dfbce339d9d",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "464ea4c8ea1ac6b55dba",
+    "url": "/React/static/css/3.902d0344.chunk.css"
+  },
+  {
+    "revision": "d195dce09931ff289844",
+    "url": "/React/static/css/4.c52f4772.chunk.css"
+  },
+  {
+    "revision": "74605c5320884cf2b0a6",
+    "url": "/React/static/css/main.0fa0161c.chunk.css"
+  },
+  {
+    "revision": "ad58a2a7a2d51a4a3055",
+    "url": "/React/static/js/2.eabc6fc0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React/static/js/2.eabc6fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464ea4c8ea1ac6b55dba",
+    "url": "/React/static/js/3.731d8fcf.chunk.js"
+  },
+  {
+    "revision": "d195dce09931ff289844",
+    "url": "/React/static/js/4.2f807d66.chunk.js"
+  },
+  {
+    "revision": "74605c5320884cf2b0a6",
+    "url": "/React/static/js/main.daddd83f.chunk.js"
+  },
+  {
+    "revision": "61fd4d40a40307edebdb",
+    "url": "/React/static/js/runtime-main.7f920461.js"
+  },
+  {
+    "revision": "64c147410011d915f18b604654787777",
+    "url": "/React/static/media/preloader.64c14741.svg"
+  },
+  {
+    "revision": "b3e90dfeb6b3b2de52eddd2c94ab19ab",
+    "url": "/React/static/media/userPhoto.b3e90dfe.png"
+  }
+]);
